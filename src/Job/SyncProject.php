8a31f6920edbe43d4fc6c679c18008d8b9ed9a16
Jim Safley <jimsafley@gmail.com>
2018-01-31 10:37:48 -0500

    Refactor SyncProject

--- src/Job/SyncProject.php
+++ src/Job/SyncProject.php
@@ -6,15 +6,23 @@ use Omeka\Job\AbstractJob;
 use Scripto\Entity\ScriptoItem;
 use Scripto\Entity\ScriptoMedia;
 
+/**
+ * Sync a Scripto project with its corresponding item set.
+ */
 class SyncProject extends AbstractJob
 {
-    /**
-     * Sync a Scripto project with its corresponding item set.
-     */
     public function perform()
     {
-        $projectId = $this->getArg('scripto_project_id');
-        $itemSetId = $this->getArg('item_set_id');
+        $this->syncProject($this->getArg('scripto_project_id'));
+    }
+
+    /**
+     * Sync a project.
+     *
+     * @param int $projectId
+     */
+    public function syncProject($projectId)
+    {
         $this->syncProjectItems($projectId);
         $this->syncProjectMedia($projectId);
     }
@@ -22,17 +30,15 @@ class SyncProject extends AbstractJob
     /**
      * Sync project items.
      *
-     * Deletes Scripto items that have been removed from the item set since the
-     * last sync; and creates Scripto items that have been assigned to the item
+     * Creates Scripto items that have been assigned to an item set since the
+     * last sync; and deletes Scripto items that have been removed from an item
      * set since the last sync.
      *
      * @param int $projectId
      */
     public function syncProjectItems($projectId)
     {
-        $services = $this->getServiceLocator();
-        $em = $services->get('Omeka\EntityManager');
-
+        $em = $this->getServiceLocator()->get('Omeka\EntityManager');
         $project = $em->find('Scripto\Entity\ScriptoProject', $projectId);
 
         // Get IDs of all items in the Scripto project.
@@ -73,23 +79,22 @@ class SyncProject extends AbstractJob
     /**
      * Sync project media.
      *
-     * Deletes Scripto media that have been removed from an item since the
-     * last sync; and creates Scripto media that have been assigned to an item
-     * since the last sync.
+     * Creates Scripto media that have been assigned to an item since the last
+     * sync; and deletes Scripto media that have been removed from an item since
+     * the last sync.
      *
      * @param int $projectId
      */
     public function syncProjectMedia($projectId)
     {
-        $services = $this->getServiceLocator();
-        $em = $services->get('Omeka\EntityManager');
+        $em = $this->getServiceLocator()->get('Omeka\EntityManager');
 
         // Iterate all Scripto items in the Scripto project.
         $sItemIds = $this->getProjectItemIds($projectId);
-        $sMediaIdsToRetain = [];
         foreach ($sItemIds as $sItemId => $itemId) {
             $item = $em->find('Omeka\Entity\Item', $itemId);
             $sItem = $em->find('Scripto\Entity\ScriptoItem', $sItemId);
+            $sMediaIdsToRetain = [];
             $position = 1;
             // Iterate all media assigned to the item.
             foreach ($this->getAllItemMedia($item) as $media) {
@@ -108,17 +113,24 @@ class SyncProject extends AbstractJob
                 }
                 $position++;
             }
+            // Delete Scripto media assigned to this Scripto item that have not
+            // been retained. Normally this step is not needed since deleting
+            // a media will cascade delete all corresponding Scripto media.
+            // Nevertheless, a bulk deletion will be necessary if the item/media
+            // abstraction ever changes.
+            $query = $em->createQuery('
+                DELETE FROM Scripto\Entity\ScriptoMedia sm
+                WHERE sm.scriptoItem = :scripto_item_id
+                AND sm.id NOT IN (:scripto_media_ids)
+            ')->setParameters([
+                'scripto_item_id' => $sItemId,
+                'scripto_media_ids' => $sMediaIdsToRetain,
+            ]);
+            $query->execute();
         }
 
-        // Delete all Scripto media that have not been retained.
-        $query = $em->createQuery('
-            DELETE FROM Scripto\Entity\ScriptoMedia sm
-            WHERE sm.id NOT IN (:scripto_media_ids)
-        ')->setParameter('scripto_media_ids', $sMediaIdsToRetain);
-        $query->execute();
-
-        // Flush the entity manager after deleting so newly persisted entities
-        // are not deleted.
+        // Must flush the entity manager after deleting so newly persisted
+        // entities are not deleted.
         $em->flush();
     }
 
