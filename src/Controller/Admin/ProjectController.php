9de6ce50bce02cae25f16c59bc28a943df5615f4
Jim Safley <jimsafley@gmail.com>
2018-02-15 15:46:51 -0500

    Add project edit and delete

--- src/Controller/Admin/ProjectController.php
+++ src/Controller/Admin/ProjectController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Scripto\Controller\Admin;
 
+use Omeka\Form\ConfirmForm;
 use Scripto\Form\ScriptoProjectForm;
 use Zend\View\Model\ViewModel;
 use Zend\Mvc\Controller\AbstractActionController;
@@ -9,9 +10,8 @@ class ProjectController extends AbstractActionController
 {
     public function addAction()
     {
-        $form = $this->getForm(ScriptoProjectForm::class, [
-            'foo' => 'bar',
-        ]);
+        $form = $this->getForm(ScriptoProjectForm::class);
+
         if ($this->getRequest()->isPost()) {
             $form->setData($this->params()->fromPost());
             if ($form->isValid()) {
@@ -35,7 +35,51 @@ class ProjectController extends AbstractActionController
 
     public function editAction()
     {
-        exit('Project::edit');
+        $form = $this->getForm(ScriptoProjectForm::class);
+        $project = $this->api()->read('scripto_projects', $this->params('project-id'))->getContent();
+
+        if ($this->getRequest()->isPost()) {
+            $form->setData($this->params()->fromPost());
+            if ($form->isValid()) {
+                $formData = $form->getData();
+                $formData['o:item_set'] = ['o:id' => $formData['o:item_set']];
+                $formData['o:property'] = ['o:id' => $formData['o:property']];
+                $response = $this->api($form)->update('scripto_projects', $this->params('project-id'), $formData);
+                if ($response) {
+                    $this->messenger()->addSuccess('Scripto project successfully edited.'); // @translate
+                    return $this->redirect()->toUrl($response->getContent()->adminUrl());
+                }
+            } else {
+                $this->messenger()->addFormErrors($form);
+            }
+        } else {
+            $data = $project->jsonSerialize();
+            $data['o:item_set'] = $data['o:item_set']->id();
+            $data['o:property'] = $data['o:property']->id();
+            $form->setData($data);
+        }
+
+        $view = new ViewModel;
+        $view->setVariable('form', $form);
+        $view->setVariable('project', $project);
+        return $view;
+    }
+
+    public function deleteAction()
+    {
+        if ($this->getRequest()->isPost()) {
+            $form = $this->getForm(ConfirmForm::class);
+            $form->setData($this->getRequest()->getPost());
+            if ($form->isValid()) {
+                $response = $this->api($form)->delete('scripto_projects', $this->params('project-id'));
+                if ($response) {
+                    $this->messenger()->addSuccess('Scripto project successfully deleted'); // @translate
+                }
+            } else {
+                $this->messenger()->addFormErrors($form);
+            }
+        }
+        return $this->redirect()->toRoute(null, ['action' => 'browse'], true);
     }
 
     public function browseAction()
--- src/Form/ScriptoProjectForm.php
+++ src/Form/ScriptoProjectForm.php
@@ -2,14 +2,16 @@
 namespace Scripto\Form;
 
 use Zend\Form\Form;
+use Omeka\Form\Element\ItemSetSelect;
+use Omeka\Form\Element\PropertySelect;
 
 class ScriptoProjectForm extends Form
 {
     public function init()
     {
         $this->add([
-            'type' => 'text',
             'name' => 'o-module-scripto:title',
+            'type' => 'text',
             'options' => [
                 'label' => 'Title', // @translate
                 'info' => 'Enter the title of this project.', // @translate
@@ -29,8 +31,8 @@ class ScriptoProjectForm extends Form
         ]);
 
         $this->add([
-            'type' => 'Omeka\Form\Element\ItemSetSelect',
             'name' => 'o:item_set',
+            'type' => ItemSetSelect::class,
             'options' => [
                 'label' => 'Item set', // @translate
                 'info' => 'Select the item set used to synchronize project items. Once synchronized, this project will contain every item in this item set.', // @translate
@@ -44,8 +46,8 @@ class ScriptoProjectForm extends Form
         ]);
 
         $this->add([
-            'type' => 'Omeka\Form\Element\PropertySelect',
             'name' => 'o:property',
+            'type' => PropertySelect::class,
             'options' => [
                 'label' => 'Property', // @translate
                 'info' => 'Select the property used to store transcriptions. Scripto stores transcriptions in a wiki before they are imported. Importing will copy transcriptions from the wiki into Omeka as values of the selected property.', // @translate
@@ -59,8 +61,8 @@ class ScriptoProjectForm extends Form
         ]);
 
         $this->add([
-            'type' => 'text',
             'name' => 'o:lang',
+            'type' => 'text',
             'options' => [
                 'label' => 'Language tag', // @translate
                 'info' => 'Enter the language of the transcriptions using an IETF language tag. Note that existing property values using the same language tag will be overwritten during import.', // @translate
