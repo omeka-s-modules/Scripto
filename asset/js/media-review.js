b8a2a18b422c3c1118bada9816ed9aaa3d805326
Jim Safley <jimsafley@gmail.com>
2018-05-09 21:26:52 -0400

    Refactor JS

--- asset/js/media-review.js
+++ asset/js/media-review.js
@@ -1,136 +1,85 @@
-(function($) {
+$(document).ready(function() {
 
-    /**
-     * Apply panzoom to an element.
-     *
-     * @param element
-     */
-    function panzoomApply(element) {
-        var container = element.parent();
-        if (!container.hasClass('image')) {
-            return;
-        }
-        $panzoom = element.panzoom({
-            $zoomIn: container.find(".zoom-in"),
-            $zoomOut: container.find(".zoom-out"),
-            $reset: container.find(".reset"),
-            maxScale: 20
-        });
-        container.on('mousewheel.focal', function(e) {
-            e.preventDefault();
-            var delta = e.delta || e.originalEvent.wheelDelta;
-            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-            $panzoom.panzoom('zoom', zoomOut, {
-                increment: 0.1,
-                animate: false,
-                focal: e
-            });
-        });
-    }
+    // Apply panzoom and featherlight.
+    if ($('.image.panzoom-container').length) {
 
-    function getRotationDegrees(obj) {
-        var matrix = obj.css("-webkit-transform") ||
-        obj.css("-moz-transform")    ||
-        obj.css("-ms-transform")     ||
-        obj.css("-o-transform")      ||
-        obj.css("transform");
-        if(matrix !== 'none') {
-            var values = matrix.split('(')[1].split(')')[0].split(',');
-            var a = values[0];
-            var b = values[1];
-            var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-        } else { var angle = 0; }
-        return (angle < 0) ? angle + 360 : angle;
-    }
+        var storedPanzoomStyle = '';
+        var storedRotateStyle = '';
 
-    function setRotation(obj, direction) {
-        var currentRotation = getRotationDegrees(obj);
-        var newRotation = (direction == 'left') ? currentRotation - 90 : currentRotation + 90;
-        obj.css('transform', 'rotate(' + newRotation + 'deg)');
-    }
+        Scripto.applyPanzoom($('#wikitext .media-render'));
 
-    $(document).ready(function() {
-        if ($('.image.panzoom-container').length) {
-
-            var storedPanzoomStyle = '';
-            var storedRotateStyle = '';
-
-            panzoomApply($('#wikitext .media-render'));
-
-            $('.full-screen').featherlight('.wikitext-featherlight', {
-                beforeOpen: function() {
-                    $('#wikitext .media-render').panzoom('destroy');
-                },
-                afterOpen: function() {
-                    panzoomApply($('.featherlight-content .media-render'));
-                },
-                beforeClose: function() {
-                    storedPanzoomStyle = $('.featherlight-content .media-render').attr('style');
-                    storedRotateStyle = $('.featherlight-content .panzoom-container img').attr('style');
-                    $('.featherlight-content .media-render').panzoom('destroy');
-                    $('#wikitext .media-render').attr('style', storedPanzoomStyle);
-                    $('#wikitext .panzoom-container img').attr('style', storedRotateStyle);
-                },
-                afterClose: function() {
-                    panzoomApply($('#wikitext .media-render'));
-                }
-            });
-    
-            $('.panzoom-container').on('click', '.rotate-left', function(e) {
-                e.preventDefault();
-                var panzoomImg = $(this).parents('.panzoom-container').find('img');
-                setRotation(panzoomImg, 'left');
-            });
-    
-            $('.panzoom-container').on('click', '.rotate-right', function(e) {
-                e.preventDefault();
-                var panzoomImg = $(this).parents('.panzoom-container').find('img');
-                setRotation(panzoomImg, 'right');
-            });
-    
-            $('.panzoom-container').on('click', '.reset', function(e) {
-                e.preventDefault();
-                var panzoomImg = $(this).parents('.panzoom-container').find('img');
-                panzoomImg.css('transform', 'none');
-            });
-        } else {
-            $('.full-screen').featherlight('.wikitext-featherlight');
-        }
-
-        function toggleExpirationSelect(protectionSelect) {
-            var expirySelect = $('[name="protection_expiry"]');
-            var currentValue = protectionSelect.find('option:selected').val();
-            if (currentValue && currentValue != "all") {
-                expirySelect.show();
-            } else {  
-                expirySelect.hide();
-            }
-        }
-
-        var protectionSelect = $('[name="protection_level"]');
-        toggleExpirationSelect(protectionSelect);
-        protectionSelect.on('change', function() {
-            toggleExpirationSelect($(this));
-        });
-
-        $('#content').on('click', '.watchlist.button', function(e) {
-            e.preventDefault();
-            var watchlistIcon = $(this);
-            $(this).toggleClass('watched')
-            var watchlistHiddenValue = $(this).next('[type="hidden"]');
-            if (watchlistHiddenValue.val() == 0) {
-                watchlistIcon.attr('aria-label', Omeka.jsTranslate('Watch media'));
-                watchlistIcon.attr('title', Omeka.jsTranslate('Watch media'));
-                watchlistHiddenValue.attr('value', 1);
-            } else {
-                watchlistIcon.attr('aria-label', Omeka.jsTranslate('Stop watching media'));
-                watchlistIcon.attr('title', Omeka.jsTranslate('Stop watching media'));
-                watchlistHiddenValue.attr('value', 0);
+        $('.full-screen').featherlight('.wikitext-featherlight', {
+            beforeOpen: function() {
+                $('#wikitext .media-render').panzoom('destroy');
+            },
+            afterOpen: function() {
+                Scripto.applyPanzoom($('.featherlight-content .media-render'));
+            },
+            beforeClose: function() {
+                storedPanzoomStyle = $('.featherlight-content .media-render').attr('style');
+                storedRotateStyle = $('.featherlight-content .panzoom-container img').attr('style');
+                $('.featherlight-content .media-render').panzoom('destroy');
+                $('#wikitext .media-render').attr('style', storedPanzoomStyle);
+                $('#wikitext .panzoom-container img').attr('style', storedRotateStyle);
+            },
+            afterClose: function() {
+                Scripto.applyPanzoom($('#wikitext .media-render'));
             }
         });
 
-        // Remove sidebar click event so revision pagination reloads the page. 
-        $('#content').off('click', '.sidebar .pagination a');
+        $('.panzoom-container').on('click', '.rotate-left', function(e) {
+            e.preventDefault();
+            var panzoomImg = $(this).parents('.panzoom-container').find('img');
+            Scripto.setRotation(panzoomImg, 'left');
+        });
+
+        $('.panzoom-container').on('click', '.rotate-right', function(e) {
+            e.preventDefault();
+            var panzoomImg = $(this).parents('.panzoom-container').find('img');
+            Scripto.setRotation(panzoomImg, 'right');
+        });
+
+        $('.panzoom-container').on('click', '.reset', function(e) {
+            e.preventDefault();
+            var panzoomImg = $(this).parents('.panzoom-container').find('img');
+            panzoomImg.css('transform', 'none');
+        });
+    } else {
+        $('.full-screen').featherlight('.wikitext-featherlight');
+    }
+
+    // Handle the layout buttons.
+    $('.layout button').click(function(e) {
+        $('.layout button').toggleClass('active');
+        $('.wikitext-featherlight').toggleClass('horizontal').toggleClass('vertical');
+        $('.layout button:disabled').removeAttr('disabled');
+        $('.layout button.active').attr('disabled', true);
     });
 
-})(jQuery)
+    // Handle the protection expiration select toggle.
+    var protectionSelect = $('[name="protection_level"]');
+    Scripto.toggleExpirationSelect(protectionSelect);
+    protectionSelect.on('change', function() {
+        Scripto.toggleExpirationSelect($(this));
+    });
+
+    // Handle the watchlist toggle.
+    $('#content').on('click', '.watchlist.button', function(e) {
+        e.preventDefault();
+        var watchlistIcon = $(this);
+        $(this).toggleClass('watched')
+        var watchlistHiddenValue = $(this).next('[type="hidden"]');
+        if (watchlistHiddenValue.val() == 0) {
+            watchlistIcon.attr('aria-label', Omeka.jsTranslate('Watch media'));
+            watchlistIcon.attr('title', Omeka.jsTranslate('Watch media'));
+            watchlistHiddenValue.attr('value', 1);
+        } else {
+            watchlistIcon.attr('aria-label', Omeka.jsTranslate('Stop watching media'));
+            watchlistIcon.attr('title', Omeka.jsTranslate('Stop watching media'));
+            watchlistHiddenValue.attr('value', 0);
+        }
+    });
+
+    // Remove sidebar click event so revision pagination reloads the page.
+    $('#content').off('click', '.sidebar .pagination a');
+});
--- asset/js/scripto.js
+++ asset/js/scripto.js
@@ -1,49 +1,112 @@
-(function($) {
-    $(document).ready(function() {
-    
-        $('.layout button').click(function(e) {
-            $('.layout button').toggleClass('active');
-            $('.wikitext-featherlight').toggleClass('horizontal').toggleClass('vertical');
-            $('.layout button:disabled').removeAttr('disabled');
-            $('.layout button.active').attr('disabled', true);
+var Scripto = {
+
+    /**
+     * Apply panzoom to an element.
+     *
+     * @param element
+     */
+    applyPanzoom: function(element) {
+        var container = element.parent();
+        if (!container.hasClass('image')) {
+            return;
+        }
+        $panzoom = element.panzoom({
+            $zoomIn: container.find(".zoom-in"),
+            $zoomOut: container.find(".zoom-out"),
+            $reset: container.find(".reset"),
+            maxScale: 20
         });
-    
-        // Handle page action menu.
-        $(document).on('o:expanded', '#page-action-menu a.collapse', function() {
-            var button = $(this);
-            $(document).on('mouseup.page-actions', function(e) {
-                var pageActionMenu = $('#page-action-menu ul');
-                if (pageActionMenu.is(e.target)) {
-                    return;
-                }
-                if (!button.is(e.target)) {
-                    button.click();
-                }
-                $(document).off('mouseup.page-actions');
+        container.on('mousewheel.focal', function(e) {
+            e.preventDefault();
+            var delta = e.delta || e.originalEvent.wheelDelta;
+            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+            $panzoom.panzoom('zoom', zoomOut, {
+                increment: 0.1,
+                animate: false,
+                focal: e
             });
         });
+    },
 
-        // Close all sidebars before opening another.
-        $('a.sidebar-content').on('click', function(e) {
-            var sidebars = $('.sidebar').each(function() {
-                Omeka.closeSidebar($(this));
-            });
-        });
+    /**
+     * Set rotation of an object.
+     *
+     * @param obj
+     * @param direction
+     */
+    setRotation: function(obj, direction) {
+        var matrix = obj.css("-webkit-transform")
+            || obj.css("-moz-transform")
+            || obj.css("-ms-transform")
+            || obj.css("-o-transform")
+            || obj.css("transform");
+        if (matrix !== 'none') {
+            var values = matrix.split('(')[1].split(')')[0].split(',');
+            var a = values[0];
+            var b = values[1];
+            var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+        } else {
+            var angle = 0;
+        }
+        var currentRotation = (angle < 0) ? angle + 360 : angle;
+        var newRotation = (direction == 'left') ? currentRotation - 90 : currentRotation + 90;
+        obj.css('transform', 'rotate(' + newRotation + 'deg)');
+    },
 
-        var drawerButtonHtml = '<button type="button" class="sidebar-drawer collapse" aria-label="' + Omeka.jsTranslate('Collapse') + '">';
-        $('.sidebar.always-open').prepend(drawerButtonHtml).addClass('expanded');
+    /**
+     * Toggle the protection expiration select.
+     *
+     * @param protectionSelect
+     */
+    toggleExpirationSelect: function(protectionSelect) {
+        var expirySelect = $('[name="protection_expiry"]');
+        var currentValue = protectionSelect.find('option:selected').val();
+        if (currentValue && currentValue != "all") {
+            expirySelect.show();
+        } else {
+            expirySelect.hide();
+        }
+    }
+};
 
-        $('.sidebar.always-open').on('click', '.sidebar-drawer', function() {
-            var drawerButton = $(this);
-            var sidebar = drawerButton.parents('.sidebar');
-            $('#content').toggleClass('expanded');
-            sidebar.toggleClass('expanded').toggleClass('collapsed');
-            if (drawerButton.hasClass('collapse')) {
-                drawerButton.removeClass('collapse').addClass('expand').attr('aria-label', Omeka.jsTranslate('Expand'));
-            } else {
-                drawerButton.removeClass('expand').addClass('collapse').attr('aria-label', Omeka.jsTranslate('Collapse'));
+$(document).ready(function() {
+
+    // Handle page action menu.
+    $(document).on('o:expanded', '#page-action-menu a.collapse', function() {
+        var button = $(this);
+        $(document).on('mouseup.page-actions', function(e) {
+            var pageActionMenu = $('#page-action-menu ul');
+            if (pageActionMenu.is(e.target)) {
+                return;
             }
+            if (!button.is(e.target)) {
+                button.click();
+            }
+            $(document).off('mouseup.page-actions');
         });
     });
-})(jQuery)
+
+    // Close all sidebars before opening another.
+    $('a.sidebar-content').on('click', function(e) {
+        var sidebars = $('.sidebar').each(function() {
+            Omeka.closeSidebar($(this));
+        });
+    });
+
+    // Handle the sidebar drawers.
+    var drawerButtonHtml = '<button type="button" class="sidebar-drawer collapse" aria-label="' + Omeka.jsTranslate('Collapse') + '">';
+    $('.sidebar.always-open').prepend(drawerButtonHtml).addClass('expanded');
+
+    $('.sidebar.always-open').on('click', '.sidebar-drawer', function() {
+        var drawerButton = $(this);
+        var sidebar = drawerButton.parents('.sidebar');
+        $('#content').toggleClass('expanded');
+        sidebar.toggleClass('expanded').toggleClass('collapsed');
+        if (drawerButton.hasClass('collapse')) {
+            drawerButton.removeClass('collapse').addClass('expand').attr('aria-label', Omeka.jsTranslate('Expand'));
+        } else {
+            drawerButton.removeClass('expand').addClass('collapse').attr('aria-label', Omeka.jsTranslate('Collapse'));
+        }
+    });
+});
 
