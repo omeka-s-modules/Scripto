f4f127fc90f8a9ea274ce6bd962c3b891abf4230
Jim Safley <jimsafley@gmail.com>
2018-05-17 13:30:50 -0400

    Implement public app item browse

---
+++ src/Controller/PublicApp/ItemController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Scripto\Controller\PublicApp;
+
+use Zend\View\Model\ViewModel;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ItemController extends AbstractActionController
+{
+    public function browseAction()
+    {
+        $project = $this->scripto()->getRepresentation($this->params('project-id'));
+        if (!$project) {
+            return $this->redirect()->toRoute('scripto');
+        }
+
+        $this->setBrowseDefaults(null);
+        $query = array_merge(
+            ['scripto_project_id' => $this->params('project-id')],
+            $this->params()->fromQuery()
+        );
+        $response = $this->api()->search('scripto_items', $query);
+        $this->paginator($response->getTotalResults(), $this->params()->fromQuery('page'));
+        $sItems = $response->getContent();
+
+        $view = new ViewModel;
+        $view->setVariable('project', $project);
+        $view->setVariable('sItems', $sItems);
+        return $view;
+    }
+
+    public function showAction()
+    {
+        return $this->redirect()->toRoute('scripto-media', ['action' => 'browse'], true);
+    }
+}
