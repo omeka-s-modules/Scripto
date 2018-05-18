7e7d003344b017ed3e83b73e2a173102f4e58948
Jim Safley <jimsafley@gmail.com>
2018-05-18 12:24:33 -0400

    Separate admin-specific JS

---
+++ asset/js/admin.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    // Handle page action menu.
+    $(document).on('o:expanded', '#page-action-menu a.collapse', function() {
+        var button = $(this);
+        $(document).on('mouseup.page-actions', function(e) {
+            var pageActionMenu = $('#page-action-menu ul');
+            if (pageActionMenu.is(e.target)) {
+                return;
+            }
+            if (!button.is(e.target)) {
+                button.click();
+            }
+            $(document).off('mouseup.page-actions');
+        });
+    });
+
+    // Close all sidebars before opening another.
+    $('a.sidebar-content').on('click', function(e) {
+        var sidebars = $('.sidebar').each(function() {
+            Omeka.closeSidebar($(this));
+        });
+    });
+
+    // Handle the sidebar drawers.
+    var drawerButtonHtml = '<button type="button" class="sidebar-drawer collapse" aria-label="' + Omeka.jsTranslate('Collapse') + '">';
+    $('.sidebar.always-open').prepend(drawerButtonHtml).addClass('expanded');
+
+    $('.sidebar.always-open').on('click', '.sidebar-drawer', function() {
+        var drawerButton = $(this);
+        var sidebar = drawerButton.parents('.sidebar');
+        $('#content').toggleClass('expanded');
+        sidebar.toggleClass('expanded').toggleClass('collapsed');
+        if (drawerButton.hasClass('collapse')) {
+            drawerButton.removeClass('collapse').addClass('expand').attr('aria-label', Omeka.jsTranslate('Expand'));
+        } else {
+            drawerButton.removeClass('expand').addClass('collapse').attr('aria-label', Omeka.jsTranslate('Collapse'));
+        }
+    });
+});
