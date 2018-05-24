3e5a7034fc9d4796efdb4887f8df1aaae3cd9b65
Jim Safley <jimsafley@gmail.com>
2018-05-24 17:15:54 -0400

    Add public revision page

--- src/Controller/PublicApp/RevisionController.php
+++ src/Controller/PublicApp/RevisionController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Scripto\Controller\PublicApp;
 
+use Scripto\Mediawiki\Exception\QueryException;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
@@ -32,6 +33,39 @@ class RevisionController extends AbstractActionController
         $view->setVariable('project', $project);
         $view->setVariable('revisions', $revisions);
         $view->setVariable('continue', $continue);
+        $this->layout()->setVariable('project', $project);
+        $this->layout()->setVariable('sItem', $sItem);
+        $this->layout()->setVariable('sMedia', $sMedia);
+        return $view;
+    }
+
+    public function showAction()
+    {
+        $sMedia = $this->scripto()->getRepresentation(
+            $this->params('project-id'),
+            $this->params('item-id'),
+            $this->params('media-id')
+        );
+        if (!$sMedia) {
+            return $this->redirect()->toRoute('scripto');
+        }
+
+        try {
+            $revision = $sMedia->pageRevision($this->params('revision-id'));
+        } catch (QueryException $e) {
+            // Invalid revision ID
+            return $this->redirect()->toRoute('admin/scripto');
+        }
+
+        $sItem = $sMedia->scriptoItem();
+        $project = $sItem->scriptoProject();
+        $view = new ViewModel;
+        $view->setVariable('revision', $revision);
+        $view->setVariable('sMedia', $sMedia);
+        $view->setVariable('media', $sMedia->media());
+        $view->setVariable('sItem', $sItem);
+        $view->setVariable('item', $sItem->item());
+        $view->setVariable('project', $project);
         $this->layout()->setVariable('project', $project);
         $this->layout()->setVariable('sItem', $sItem);
         $this->layout()->setVariable('sMedia', $sMedia);
