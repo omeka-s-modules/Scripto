38c582eaccf846ccc979398859639b23954b7e13
Jim Safley <jimsafley@gmail.com>
2018-05-25 16:31:41 -0400

    Add public compare revisions

--- src/Controller/PublicApp/RevisionController.php
+++ src/Controller/PublicApp/RevisionController.php
@@ -71,4 +71,39 @@ class RevisionController extends AbstractActionController
         $this->layout()->setVariable('sMedia', $sMedia);
         return $view;
     }
+
+    public function compareAction()
+    {
+        $sMedia = $this->scripto()->getRepresentation(
+            $this->params('project-id'),
+            $this->params('item-id'),
+            $this->params('media-id')
+        );
+        if (!$sMedia) {
+            return $this->redirect()->toRoute('admin/scripto');
+        }
+
+        $fromRevision = $sMedia->pageRevision($this->params('from-revision-id'));
+        $toRevision = $sMedia->pageRevision($this->params('to-revision-id'));
+        $compare = $this->scripto()->apiClient()->compareRevisions(
+            $this->params('from-revision-id'),
+            $this->params('to-revision-id')
+        );
+
+        $sItem = $sMedia->scriptoItem();
+        $project = $sItem->scriptoProject();
+        $view = new ViewModel;
+        $view->setVariable('fromRevision', $fromRevision);
+        $view->setVariable('toRevision', $toRevision);
+        $view->setVariable('compare', $compare);
+        $view->setVariable('sMedia', $sMedia);
+        $view->setVariable('media', $sMedia->media());
+        $view->setVariable('sItem', $sItem);
+        $view->setVariable('item', $sItem->item());
+        $view->setVariable('project', $project);
+        $this->layout()->setVariable('project', $project);
+        $this->layout()->setVariable('sItem', $sItem);
+        $this->layout()->setVariable('sMedia', $sMedia);
+        return $view;
+    }
 }
