35f50691733256bcf70eb32a311617e98161354b
Jim Safley <jimsafley@gmail.com>
2018-06-15 14:22:53 -0400

    Improve sort and filter menu behavior

--- asset/js/public-app.js
+++ asset/js/public-app.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 
+// Toggle the sort and filter menus.
 $('.menu-toggle').on('click', function(e) {
     e.preventDefault();
+    e.stopPropagation();
     var thisMenu = $(this).parent();
-    thisMenu.toggleClass('open');
+    // Set a high z-index so subsequent in-menu clicks don't fall through.
+    thisMenu.toggleClass('open').css('z-index', 100);
     if (thisMenu.hasClass('sorting-toggle')) {
         $('.filtering-toggle').removeClass('open');
     } else if (thisMenu.hasClass('filtering-toggle')) {
         $('.sorting-toggle').removeClass('open');
     }
 });
+// Close the sort and filter menus by clicking anywhere outside them.
+$(document).on('click', function(e) {
+    if ($(e.target).parent().parent().is('.sorting-toggle, .filtering-toggle')) {
+        // Ignore if clicks are triggered from in-menu selects.
+        return;
+    }
+    $('.filtering-toggle, .sorting-toggle').removeClass('open');
+});
 
+// Handle the list and grid buttons.
 var listButton = $('.list-layout .list');
 var gridButton = $('.list-layout .grid');
-
 listButton.click(function() {
     $('.resource-list table').show();
     $('.resource-list .resource-grid').hide();
