Daniel-KM/Omeka-S-module-Psl

Added a form element to select media types.

src/Form/Element/MediaTypeSelect.php
@@ -1,0 +1,8 @@
+<?php
+namespace Psl\Form\Element;
+
+use Zend\Form\Element\Select;
+
+class MediaTypeSelect extends Select
+{
+}

src/Service/Form/Element/MediaTypeSelectFactory.php
@@ -1,0 +1,34 @@
+<?php
+namespace Psl\Service\Form\Element;
+
+use Psl\Form\Element\MediaTypeSelect;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class MediaTypeSelectFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        $list = $this->listMediaTypes($services);
+
+        $element = new MediaTypeSelect;
+        return $element
+            ->setValueOptions($list)
+            ->setEmptyOption('Select media type…'); // @translate
+    }
+
+    protected function listMediaTypes(ContainerInterface $services)
+    {
+        $connection = $services->get('Omeka\Connection');
+        $sql = <<<SQL
+SELECT DISTINCT(media_type)
+FROM media
+WHERE media_type IS NOT NULL
+AND media_type != ""
+ORDER BY media_type
+SQL;
+        $stmt = $connection->query($sql);
+        $result = $stmt->fetchAll(\PDO::FETCH_COLUMN);
+        return array_combine($result, $result);
+    }
+}

omeka-s-modules/Scripto

src/Form/Element/MediaTypeSelect.php
@@ -1,0 +1,8 @@
+<?php
+namespace Scripto\Form\Element;
+
+use Zend\Form\Element\Select;
+
+class MediaTypeSelect extends Select
+{
+}

src/Service/Form/Element/MediaTypeSelectFactory.php
@@ -1,0 +1,34 @@
+<?php
+namespace Scripto\Service\Form\Element;
+
+use Scripto\Form\Element\MediaTypeSelect;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class MediaTypeSelectFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        $list = $this->listMediaTypes($services);
+
+        $element = new MediaTypeSelect;
+        return $element
+            ->setValueOptions($list)
+            ->setEmptyOption('Select media type…'); // @translate
+    }
+
+    protected function listMediaTypes(ContainerInterface $services)
+    {
+        $connection = $services->get('Omeka\Connection');
+        $sql = <<<SQL
+SELECT DISTINCT(media_type)
+FROM media
+WHERE media_type IS NOT NULL
+AND media_type != ""
+ORDER BY media_type
+SQL;
+        $stmt = $connection->query($sql);
+        $result = $stmt->fetchAll(\PDO::FETCH_COLUMN);
+        return array_combine($result, $result);
+    }
+}