8365a53f98c59d9b429ee02e106a9fcf1c6b1747
Jim Safley <jimsafley@gmail.com>
2021-01-06 15:07:36 -0500

    Remove obsolete functions

--- asset/js/scripto.js
+++ asset/js/scripto.js
@@ -1,63 +1,6 @@
 var Scripto = {
 
     /**
-     * Apply panzoom to an element.
-     *
-     * @param element
-     */
-    applyPanzoom: function(element) {
-        var container = element.parentNode;
-        
-        if ((" " + container.className + " ").replace(/[\n\t]/g, " ").indexOf(" image ") < 0 ) {
-            return;
-        }
-
-        $panzoom = Panzoom(element, {
-            maxScale: 20
-        });
-        
-        var zoomInButton = document.getElementsByClassName('zoom-in')[0];
-        var zoomOutButton = document.getElementsByClassName('zoom-out')[0];
-        zoomInButton.addEventListener('click', $panzoom.zoomIn);
-        zoomOutButton.addEventListener('click', $panzoom.zoomOut);
-        /* container.on('mousewheel.focal', function(e) {
-            e.preventDefault();
-            var delta = e.delta || e.originalEvent.wheelDelta;
-            var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-            $panzoom.panzoom('zoom', zoomOut, {
-                increment: 0.1,
-                animate: false,
-                focal: e
-            });
-        }); */
-    },
-
-    /**
-     * Set rotation of an object.
-     *
-     * @param obj
-     * @param direction
-     */
-    setRotation: function(obj, direction) {
-        var matrix = obj.css("-webkit-transform")
-            || obj.css("-moz-transform")
-            || obj.css("-ms-transform")
-            || obj.css("-o-transform")
-            || obj.css("transform");
-        if (matrix !== 'none') {
-            var values = matrix.split('(')[1].split(')')[0].split(',');
-            var a = values[0];
-            var b = values[1];
-            var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-        } else {
-            var angle = 0;
-        }
-        var currentRotation = (angle < 0) ? angle + 360 : angle;
-        var newRotation = (direction == 'left') ? currentRotation - 90 : currentRotation + 90;
-        obj.css('transform', 'rotate(' + newRotation + 'deg)');
-    },
-
-    /**
      * Toggle the protection expiration select.
      *
      * @param protectionSelect
