a1ba3d3775153bb75f757ed10bb7a0be16b1e909
Kim Nguyen <design@kimisgold.com>
2021-01-08 21:08:18 +0000

    Restore watchlist toggle.

--- asset/js/common/media.js
+++ asset/js/common/media.js
@@ -160,4 +160,25 @@ function handleWatchlistButton() {
     });
 }
 
+// Handle the watchlist toggle.
+var watchlist = $('.watch-list');
+var watchedIcon = watchlist.children('.watchlist.button.watched');
+var notWatchedIcon = watchlist.children('.watchlist.button').not('.watched');
+var watching = watchlist.data('watching');
+
+watchlist.children('.watchlist.button').on('click', function(e) {
+    e.preventDefault();
+    watching = (1 === watching) ? 0 : 1;
+    $.post(watchlist.data('url'), {'watching': watching})
+        .done(function(data) {
+            watchedIcon.toggle();
+            notWatchedIcon.toggle();
+            if (watching) {
+                watchlist.children('.watch.success').fadeIn('slow').delay(2000).fadeOut('slow');
+            } else {
+                watchlist.children('.unwatch.success').fadeIn('slow').delay(2000).fadeOut('slow');
+            }
+        });
+});
+
 });
